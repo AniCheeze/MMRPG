ef710dbcb0d839db82d7036d1c28c57b4e859934
AniCheeze <daniil.avd06@mail.ru>
2025-01-27 18:29:01 +0700

    Interface of Registration

--- WpfApp11/MainWindow.xaml.cs
+++ WpfApp11/MainWindow.xaml.cs
@@ -19,6 +19,8 @@ namespace WpfApp11
         public MainWindow()
         {
             InitializeComponent();
+            LinearGradientBrush gradientBrush = new LinearGradientBrush(Color.FromRgb(81, 225, 255), Color.FromRgb(11, 60, 196), new Point(0.5, 0), new Point(0.5, 1));
+            Background = gradientBrush;
         }
     }
 }
